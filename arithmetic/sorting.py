dc796a46ba8240d2aa5dd115734941fa7029342d
Wang,Liuying <13656036646@163.com>
2018-03-16 18:15:32 +0800

    新学了几种排序
    插入 希尔 快速 堆

--- arithmetic/sorting.py
+++ arithmetic/sorting.py
@@ -4,6 +4,10 @@
 选择排序
 冒泡排序
 归并排序
+插入排序
+希尔排序
+快速排序
+堆排序
 
 Created on Fri Mar  9 22:07:57 2018
 
@@ -99,7 +103,6 @@ a = [2,4]
 b = [1,3]
 merge(a,b)
 
-#%% final box
 def ssort(alist):
     if len(alist)==1:
         return alist
@@ -114,3 +117,117 @@ def ssort(alist):
 #test
 a = [1,3,2,4]
 ssort(a)
+
+#%%插入排序
+"""
+从arr的第1位开始向后遍历：
+ 将第一位与第零位比较，排序
+ 将第二位与第一位比较排序，再与第零位比较
+ ……
+"""
+def insertSort(a):
+    for i in range(1,len(a)):
+        while i>0 and a[i]<a[i-1]:
+            a[i],a[i-1]=a[i-1],a[i]
+            i -= 1
+        print(a)
+        
+a = [4,2,1,6,3,4]
+insertSort(a)
+
+#%%快速排序
+"""
+从数组中随机选择一位数，将比它大的放右边，比它小的放左边
+然后对左和右递归调用
+"""
+#import pdb
+
+class QuickSort:
+    a = []
+    def __init__(self, alist):
+        self.a = alist
+        
+    def quickSort(self,start = 0, end = None):
+        #pdb.set_trace()
+        if end==None:
+            end = len(self.a)-1
+        if len(self.a[start:end+1])<=1:
+            return None
+        partition = self.a[end]
+        pointer = start
+        for i in range(start,end+1):
+            if self.a[i]<partition:
+                self.a[i],self.a[pointer] = self.a[pointer],self.a[i]
+                pointer += 1
+        self.a[pointer],self.a[end] = self.a[end],self.a[pointer]
+        print(self.a)
+        self.quickSort(start = start, end = pointer-1)
+        self.quickSort(start = pointer+1, end = end)
+        return None
+    
+b = QuickSort([4,2,1,6,3,4])
+b.quickSort()
+#想了很久，不太懂python的函数传递机制，只能想到定义一个类，对类中的self.a进行修改的这种方式
+
+
+
+#%%希尔排序
+"""
+希尔排序(Shell Sort)是插入排序的一种。
+也称缩小增量排序，是直接插入排序算法的一种更高效的改进版本。
+希尔排序是非稳定排序算法。
+ 希尔排序是把记录按下标的一定增量分组，对每组使用直接插入排序算法排序；
+ 随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，
+ 整个文件恰被分成一组，算法便终止。
+"""
+   
+def shellSort(a,step):
+    while step>0:
+        for i in range(step,len(a)):
+            while i-step>=0 and a[i]<a[i-step]:
+                a[i],a[i-step] = a[i-step],a[i]
+                i -= step
+        step -= 1
+        
+b = [4,2,1,6,3,4]
+shellSort(b,2)
+b
+
+#%%堆排序
+"""
+堆排序(Heapsort)是指利用堆积树（堆）这种数据结构所设计的一种排序算法
+，它是选择排序的一种。
+可以利用数组的特点快速定位指定索引的元素。
+堆分为大根堆和小根堆，是完全二叉树。
+大根堆的要求是每个节点的值都不大于其父节点的值，
+即A[PARENT[i]] >= A[i]。在数组的非降序排序中，需要使用的就是大根堆，
+因为根据大根堆的要求可知，最大的值一定在堆顶。
+"""
+def adjustHeap(a, nodeId, n):
+     #n最大结点序号
+    lchild = nodeId*2+1
+    rchild = nodeId*2+2 #左右子结点的ID
+    largest = nodeId #假设这个分支结点是最大的
+    if lchild<=n and a[lchild]>a[nodeId]:
+        largest = lchild
+    if rchild<=n and a[rchild]>a[largest]:
+        largest = rchild                  #找到三点中最大的
+    if nodeId!=largest: #如果分支不是最大的
+        a[nodeId],a[largest] = a[largest],a[nodeId]
+
+def findMax(a,n):
+    for node in range(int((n-1)/2),-1,-1):
+        adjustHeap(a,node,n)
+
+def heapSort(a):
+    for i in range(len(a))[::-1]:
+        findMax(a,i)
+        a[0],a[i] = a[i],a[0]
+    print(a)
+        
+a = [1,4,2,5,6,3,7]
+heapSort(a)        
+
+
+#%%draft
+a = [1,4,2,5,6,3,7]
